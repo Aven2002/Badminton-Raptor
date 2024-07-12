ef7a3db42506e2ee83401ab9e5a4ab03e828f2a2
AvenDing <139747334+Aven2002@users.noreply.github.com>
2024-07-12 14:38:02 +0800

    Update Landing page

--- src/views/Landing_view.vue
+++ src/views/Landing_view.vue
@@ -1,30 +1,30 @@
 <template>
   <div class="landing">
-    <h1>Welcome to the Landing Page</h1>
-    <p>This is the landing page content.</p>
+    <div class="body">
+      <!-- Main content section -->
+      <div class="content">
+        <h2>Experience the Power of Badminton Raptor</h2>
+        <p>
+          Encounter the Swiftness, Accuracy, and Simplicity of Badminton Raptor.
+          Whether delving into singular equipment alternatives or researching
+          contemporary gear trends, empower your decision-making prowess.
+        </p>
+        <ul class="feature-list">
+          <li>🌐 Streamlined Equipment Exploration</li>
+          <li>⏱️ Personalized Equipment Recommendations</li>
+          <li>💼 Comprehensive Equipment Database</li>
+          <li>🌍 Facilitate Informed Decision-making</li>
+        </ul>
+        <div class="button mt-4">
+          <router-link to="/Login_view" class="btn btn-primary me-2">Login</router-link>
+          <router-link to="/SignUp_view" class="btn btn-success">Sign Up</router-link>
+        </div>
+      </div>
 
-    <!-- Main content section -->
-    <div class="content">
-      <h2>Experience the Power of Badminton Raptor</h2>
-      <p>
-        Encounter the Swiftness, Accuracy, and Simplicity of Badminton Raptor.
-        Whether delving into singular equipment alternatives or researching
-        contemporary gear trends, empower your decision-making prowess.
-      </p>
-      <h3>Key Features:</h3>
-      <text>🌐 Streamlined Equipment Exploration</text>
-      <text>⏱️ Personalized Equipment Recommendations</text>
-      <text>💼 Comprehensive Equipment Database</text>
-      <text>🌍 Facilitate Informed Decision-making</text>
-    </div>
-
-    <div class="button mt-4">
-      <router-link to="/Login_view" class="btn btn-primary me-2"
-        >Login</router-link
-      >
-      <router-link to="/SignUp_view" class="btn btn-success"
-        >Sign Up</router-link
-      >
+      <!-- Image on the right side -->
+      <div class="image-container">
+        <img src="../assets/background/Bg_logo.png" alt="background_logo">
+      </div>
     </div>
   </div>
 </template>
@@ -36,10 +36,27 @@ export default {
 </script>
 
 <style>
+.body {
+  display: flex;
+  justify-content: center; /* Center content horizontally */
+  align-items: center; /* Center content vertically */
+}
+
+.landing {
+  margin: auto; 
+  background-color: black; 
+}
+.feature-list {
+  list-style-type: none; 
+  padding-left:0;
+}
 .content {
-  max-width: 800px; /* Adjust as needed */
-  margin: auto;
-  padding: 20px;
+  max-width: 1000px; 
   text-align: left;
+  color: white; 
+}
+
+.image-container img {
+  max-width: 100%; 
 }
 </style>
