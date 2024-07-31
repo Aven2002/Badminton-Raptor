e06c735de362b3b68f5545ef46cbd5923c1a34e7
AvenDing <139747334+Aven2002@users.noreply.github.com>
2024-07-31 16:05:49 +0800

    Split folder - Admin views and User views

--- src/router/index.js
+++ src/router/index.js
@@ -2,19 +2,20 @@ import { createRouter, createWebHistory } from 'vue-router';
 import stateManagement from '@/State_management';
 
 // Import views
-import Landing_view from '@/views/Landing_view.vue';
-import Home_view_user from '@/views/Home_view_user.vue';
-import Login_view from '@/views/Login_view.vue';
-import SignUp_view from '@/views/SignUp_view.vue';
-import Browse_Equipment_view from '@/views/Browse_Equipment_view.vue';
-import Compare_Equipment_view from '@/views/Compare_Equipment_view.vue';
-import Personalized_Recommendation_view from '@/views/Personalized_Recommendation_view.vue';
-import Settings_view from '@/views/Settings_view.vue';
-import Favorite_List_view from '@/views/Favorite_List_view.vue';
-import Contact_Us_view from '@/views/Contact_Us_view.vue';
-import Reset_Password_view from '@/views/Reset_Password_view.vue';
-import Equipment_Details_view from '@/views/Equipment_Details_view.vue';
-import Home_view_admin from '@/views/Home_view_admin.vue';
+import Landing_view from '@/views/User_views/Landing_view.vue';
+import Home_view_user from '@/views/User_views/Home_view_user.vue';
+import Login_view from '@/views/User_views/Login_view.vue';
+import SignUp_view from '@/views/User_views/SignUp_view.vue';
+import Browse_Equipment_view from '@/views/User_views/Browse_Equipment_view.vue';
+import Compare_Equipment_view from '@/views/User_views/Compare_Equipment_view.vue';
+import Personalized_Recommendation_view from '@/views/User_views/Personalized_Recommendation_view.vue';
+import Settings_view from '@/views/User_views/Settings_view.vue';
+import Favorite_List_view from '@/views/User_views/Favorite_List_view.vue';
+import Contact_Us_view from '@/views/User_views/Contact_Us_view.vue';
+import Reset_Password_view from '@/views/User_views/Reset_Password_view.vue';
+import Equipment_Details_view from '@/views/User_views/Equipment_Details_view.vue';
+
+import Home_view_admin from '@/views/Admin_views/Home_view_admin.vue';
 
 const routes = [
   // Define routes
@@ -30,6 +31,7 @@ const routes = [
   { path: "/Contact_Us_view", name: "Contact_Us_view", component: Contact_Us_view, meta: { requiresUser: true } },
   { path: "/Reset_Password_view", name: "Reset_Password_view", component: Reset_Password_view },
   { path: '/equipment/:id', name: 'Equipment_Details_view', component: Equipment_Details_view, props: true },
+
   { path: "/Home_view_admin", name: "Home_view_admin", component: Home_view_admin, meta: { requiresAdmin: true } }
 ];
 
